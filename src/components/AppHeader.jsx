150bf3def20240c02cd86d26662264a720e15c7a
FrancescoTullio <francescotullio1992@gmail.com>
2026-01-03 12:04:26 +0100

    Update README, enhance AppHeader component, and add image assets

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,11 +1,58 @@
 import { Link } from "react-router-dom";
 
 function AppHeader() {
-    return (
-        <header className="bg-dark text-white p-3">
-            sono l'header
-        </header>
-    );
-};
+  return (
+    <header className="p-1">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary bg-primary" data-bs-theme="light">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo03"
+            aria-controls="navbarTogglerDemo03"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <a className="navbar-brand" href="#">
+            <img src="public\ChatGPT Image 3 gen 2026, 11_54_47.png" alt="" />
+          </a>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Link
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled" aria-disabled="true">
+                  Disabled
+                </a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-secondary" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
 
 export default AppHeader;
