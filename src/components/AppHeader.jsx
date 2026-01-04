cba23acd8009be02218baf87476e687122e0f3dd
FrancescoTullio <francescotullio1992@gmail.com>
2026-01-04 11:15:53 +0100

    Refactor AppHeader component to use consistent Link syntax and remove unused search form

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -21,7 +21,7 @@ function AppHeader() {
             <span className="navbar-toggler-icon"></span>
           </button>
           {/* CORREZIONE 1: Usa Link per il brand e correggi il percorso immagine */}
-          <Link className="navbar-brand" to="/"> 
+          <Link className="navbar-brand" to="/">
             <img
               src="/logo_footer.png" // Ho rimosso 'public\' e usato '/'
               alt="Logo"
@@ -32,7 +32,7 @@ function AppHeader() {
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 {/* CORREZIONE 2: Usa solo Link e applica le classi Bootstrap direttamente */}
-                <Link className="nav-link active" aria-current="page" to="/"> 
+                <Link className="nav-link active" aria-current="page" to="/">
                   Home
                 </Link>
               </li>
@@ -43,17 +43,6 @@ function AppHeader() {
                 </Link>
               </li>
             </ul>
-            <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-secondary" type="submit">
-                Search
-              </button>
-            </form>
           </div>
         </div>
       </nav>
